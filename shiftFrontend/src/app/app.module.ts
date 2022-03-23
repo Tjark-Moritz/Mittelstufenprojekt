91113b368ee1147e126e58722ea864aabcf21045
Lennart Mikutis <lennart.m2@schule.bremen.de>
2022-03-23 14:53:54 +0100

    Created notification bell. Creaded Navbar with Icon and Notification_Bell.

--- shiftFrontend/src/app/app.module.ts
+++ shiftFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
