995ed03abb7f4ba62bb097bb66b87327121a0716
Tjark <tjark24qschule.bremen.de>
2022-03-24 12:59:50 +0100

    changed search pipe

--- shiftFrontend/src/app/app.module.ts
+++ shiftFrontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { HighliterPipe } from './pipes/highliter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    HighliterPipe
   ],
   imports: [
     BrowserModule,
