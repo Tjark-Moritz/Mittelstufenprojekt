51683e9507055d2dd78aaeeb564d5d6773840dd4
Lennart Mikutis <lennart.m2@schule.bremen.de>
2022-04-05 13:39:32 +0200

    Added Login UI.

--- shiftFrontend/src/app/app.module.ts
+++ shiftFrontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
