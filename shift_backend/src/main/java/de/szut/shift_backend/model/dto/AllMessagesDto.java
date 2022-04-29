30fb282e1994291b9305d1a18c551905dd52f18f
Fynn Luca Peschel <fynn-luca.peschel@hmmh.de>
2022-04-29 15:09:53 +0200

    LF12-181: [intermediate state] added Message-Entity and Dto´s

---
+++ shift_backend/src/main/java/de/szut/shift_backend/model/dto/AllMessagesDto.java
@@ -0,0 +1,10 @@
+package de.szut.shift_backend.model.dto;
+
+public class AllMessagesDto {
+
+    private GetMessageDto getMessageDto;
+
+    public AllMessagesDto(GetMessageDto getMessageDto) {
+        this.getMessageDto = getMessageDto;
+    }
+}
