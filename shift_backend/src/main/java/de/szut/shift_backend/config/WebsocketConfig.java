38bc2cc809f3ae4444f374610747251d38dcb055
fynn-luca.peschel <fynn194@schule.bremen.de>
2022-05-13 11:55:12 +0200

    LF12-192: fixed WebSocket connection

--- shift_backend/src/main/java/de/szut/shift_backend/config/WebsocketConfig.java
+++ shift_backend/src/main/java/de/szut/shift_backend/config/WebsocketConfig.java
@@ -1,24 +1,34 @@
 package de.szut.shift_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@CrossOrigin
+class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/trash/chat");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
-                .setHandshakeHandler((serverHttpRequest, serverHttpResponse, webSocketHandler, map) -> true);
+                .setAllowedOrigins("http://localhost:8090", "chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
+                .setHandshakeHandler(new DefaultHandshakeHandler());
+
+
+        registry.addEndpoint("/chat")
+                .withSockJS();
     }
 }
--- shift_backend/src/main/java/de/szut/shift_backend/controller/MessageHandlingController.java
+++ shift_backend/src/main/java/de/szut/shift_backend/controller/MessageHandlingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/trash")
+@MessageMapping("/trash")
 public class MessageHandlingController {
 
     private final MappingService mappingService;
