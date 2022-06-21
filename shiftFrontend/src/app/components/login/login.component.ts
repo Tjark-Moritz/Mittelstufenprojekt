695c4755c32742ccedd6b4f36b44ca2d2d80e3b5
Lennart Mikutis <lennart.m2@schule.bremen.de>
2022-06-21 20:35:01 +0200

    Removed unused wrong login.

--- shiftFrontend/src/app/components/login/login.component.ts
+++ shiftFrontend/src/app/components/login/login.component.ts
@@ -11,7 +11,6 @@ import {LoginService} from "../../services/login.service";
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  wrongLogin: boolean = false;
   private autoLoginChecked: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private loginService: LoginService, private bearerTokenService: BearerTokenService) {
