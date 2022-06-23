76b355ff9664037a9f1b80b356eb721c3525888c
Tjark <tjark24qschule.bremen.de>
2022-06-23 12:59:47 +0200

    department fixed

--- shiftFrontend/src/app/models/dto/AddDepartment.ts
+++ shiftFrontend/src/app/models/dto/AddDepartment.ts
@@ -1,7 +1,10 @@
+import {AddShiftType} from "./AddShiftType";
+
 export class AddDepartment {
   constructor(public name?: string,
               public abbreviatedName?: string,
               public leadEmployeeId?: number,
-              public employeeIds?: number[]) {
+              public employeeIds?: number[],
+              public shiftTypes?: AddShiftType[]) {
   }
 }
