f04bb440d4038c87107338d99e61e70ef23ed6af
Christian Kämmerer <christian.k4@schule.bremen.de>
2022-06-24 21:48:57 +0200

    [LF12-266] Shifts now correctly get swapped in shifplan

--- shift_backend/src/main/java/de/szut/shift_backend/services/ShiftService.java
+++ shift_backend/src/main/java/de/szut/shift_backend/services/ShiftService.java
@@ -1,6 +1,7 @@
 package de.szut.shift_backend.services;
 
 import de.szut.shift_backend.exceptionHandling.ResourceNotFoundException;
+import de.szut.shift_backend.model.Employee;
 import de.szut.shift_backend.model.Shift;
 import de.szut.shift_backend.repository.ShiftRepository;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,16 @@ public class ShiftService {
             throw new ResourceNotFoundException("Shift with id '" + shiftId + "'could not be found");
 
         return shift.get();
+    }
+
+    public void swapShifts(Employee requestingEmployee, Shift oldShift, Employee replyingEmployee, Shift newShift) {
+        oldShift.getActiveEmployees().remove(requestingEmployee);
+        oldShift.getActiveEmployees().add(replyingEmployee);
+
+        newShift.getActiveEmployees().remove(replyingEmployee);
+        newShift.getActiveEmployees().add(requestingEmployee);
+
+        this.shiftRepository.save(oldShift);
+        this.shiftRepository.save(newShift);
     }
 }
